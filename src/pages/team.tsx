cfc5725adfe6072071554df7dfcc61dea7096eea
Sabaneak <akhilmacherla8@gmail.com>
2022-01-29 01:29:15 +0530

    Team Cards

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -1,10 +1,19 @@
 import { NextPage } from 'next';
 import React from 'react';
+import TeamCard from '../components/TeamCard';
 
 const TeamPage: NextPage = ({}) => {
 	return (
 		<>
-			<h1 className='font-extrabold text-[54px] mb-10 ml-40'>Team</h1>
+			<div>
+				<h1 className='font-extrabold text-[54px] mb-10 ml-40'>Team</h1>
+				<div className='max-w-6xl mx-auto gap-3 grid grid-cols-3 lg:pb-6'>
+					<TeamCard coverPic='/assets/rick.jpg' name='Sreekar' subtitle='Thope'/>
+					<TeamCard coverPic='/assets/rick.jpg' name='Sreekar' subtitle='Thope'/>
+					<TeamCard coverPic='/assets/rick.jpg' name='Sreekar' subtitle='Thope'/>
+				</div>
+			</div>
+			
 		</>
 	);
 };
